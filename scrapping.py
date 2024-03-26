64ef53f356e8e0e34fb534c7269b511ce313dd90
Ryan Rodrigo <ryanr4057@gmail.com>
2024-03-26 12:35:29 -0300

    aaa

--- scrapping.py
+++ scrapping.py
@@ -15,19 +15,19 @@ def scroll_to(driver, element):
             .perform()
 
 def acessa_pista(i):
-    pistas = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@data-eventid='cards_meetings_click']")))
     try:
-        time.sleep(1)
+        pistas = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@data-eventid='cards_meetings_click']")))
         scroll_to(driver, pistas[i])
         wait.until(EC.element_to_be_clickable(pistas[i]))
+        time.sleep(1.1)
         pistas[i].click()
-    except ElementClickInterceptedException:
-        scroll_to(driver, pistas[i])
-        pistas[i].click()
-    races = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@data-eventid='cards_card']")))
-    print(driver.find_element(By.ID, "meetingHeaderTitle").text)
-    print(len(races))
-    driver.back()
+        races = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@data-eventid='cards_card']")))
+        print(driver.find_element(By.ID, "meetingHeaderTitle").text)
+        print(len(races))
+        driver.back()
+        return False
+    except Exception as e:
+        return True
 
 options = Options()
 options.add_argument('--headless')
@@ -46,19 +46,19 @@ driver.maximize_window()
 driver.get("https://greyhoundbet.racingpost.com/#meeting-list/r_date=2024-03-26")
 
 
-# driver.execute_script('''
-#     var style = document.createElement('style');
-#     style.innerHTML = '*, *::before, *::after { animation-play-state: paused !important; }';
-#     document.head.appendChild(style);
-# ''')
-# wait.until(EC.invisibility_of_element_located((By.ID, "webAppLanding")))
+driver.execute_script('''
+    var style = document.createElement('style');
+    style.innerHTML = '*, *::before, *::after { animation-play-state: paused !important; }';
+    document.head.appendChild(style);
+''')
+wait.until(EC.invisibility_of_element_located((By.ID, "webAppLanding")))
 
 pista = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@data-eventid='cards_meetings_click']")))
 
 for i in range(len(pista)):
-    try:
-        acessa_pista(i)
-    except StaleElementReferenceException:
-        acessa_pista(i)
+    result = acessa_pista(i)
+    while result:
+        result = acessa_pista(i)
+        
 
     
